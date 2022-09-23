6e6c39a58a4aa2fcc58e6a77b33746e5ee13daf8
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-09-23 13:42:05 -0500

    [ADDED] Login Systems

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,88 @@
 const express = require("express"),
     router = express.Router(),
-    { getDatabase, ref, onValue } = require("@firebase/database"),
+    { set, getDatabase, ref, onValue } = require("@firebase/database"),
     fs = require("fs"),
-    xml = fs.readFileSync(__dirname + '/../public/sitemap.xml');
+    xml = fs.readFileSync(__dirname + '/../public/sitemap.xml'),
+    bcrypt = require("bcryptjs"),
+    passport = require("passport");
+
+const { authenticationMiddleware } = require("../auth/functions/middlewares")
+
+router.get("/sitemap.xml", function (req, res, next) {
+    res.set("Content-Type", "text/xml");
+    res.send(xml);
+});
 
 router.get("/", (req, res) => {
     res.render("pages/login");
 });
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-});
+router.post(
+    "/",
+    passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "?message=credentialserror",
+    })
+);
 
 router.get("/registro", (req, res) => {
     res.render("pages/login/registro.ejs");
 });
 
 router.post("/registro", (req, res) => {
-    console.log(req.body);
+    const data = req.body;
+    console.log(data);
+    if (data.password !== data.passwordConf) return res.redirect('?message=passwordsdontmatch');
+    if (data['g-recaptcha-response'] === '') return res.redirect('?message=errorrecaptcha');
+    const db = getDatabase();
+    const users = ref(db, "gestaoempresa/empresa");
+    onValue(users, async (snapshot) => {
+      let allUsers;
+      if (snapshot.val() === null) {
+        allUsers = [];
+      } else {
+        allUsers = snapshot.val();
+      };
+      const user = {
+        _id: data.email,
+        email: data.email,
+        password: bcrypt.hashSync(data.password),
+        verified: false,
+        documents: {
+          nome_fantasia: data.nomeF,
+          cnpj: data.cnpj,
+        },
+        contact: {
+          number: ""
+        },
+        contractURL: ""
+      };
+  
+      const checkUnique = () => {
+        return allUsers.find((item) => item.email === user.email);
+      };
+  
+      if (checkUnique())
+        return res.redirect('/?fail=true&message=userexists');
+  
+      allUsers.push(user);
+      set(ref(db, "gestaoempresa/empresa"), allUsers);
+  
+      return res.redirect("/?message=registered");
+    });
+});
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+
+router.get("/dashboard", (req, res, next) => {
+    authenticationMiddleware(req, res, next)
+    res.render("pages/dashboard");
 });
 
 
