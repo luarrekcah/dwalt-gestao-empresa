e90b02c6f957a6d48df356d0a3b08f0fb2ca040f
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-09-27 19:47:29 -0500

    [ADDED] Button on gen projects, added page projects with the inputs

--- routes/projects.js
+++ routes/projects.js
@@ -14,4 +14,15 @@ router.get("/", (req, res, next) => {
     }
 });
 
+router.get("/adicionar", (req, res, next) => {
+    const data = {
+        user: req.user,
+    };
+    if (authenticationMiddlewareTrueFalse(req, res, next)) {
+        res.render("pages/projects/new", data);
+    } else {
+        res.redirect("/");
+    }
+});
+
 module.exports = router;
