6e80cfff701c65610f4ebba162d5dde5248ea7dd
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-09-27 20:34:59 -0500

    [ADDED] Post route to register user

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,7 @@
 const express = require("express"),
-    router = express.Router();
-    
+    router = express.Router(),
+    { set, getDatabase, ref, onValue } = require("@firebase/database");
+
 const { authenticationMiddleware, authenticationMiddlewareTrueFalse } = require("../auth/functions/middlewares")
 
 router.get("/", (req, res, next) => {
@@ -23,6 +24,25 @@ router.get("/adicionar", (req, res, next) => {
     } else {
         res.redirect("/");
     }
+});
+
+router.post("/adicionar", (req, res, next) => {
+    let allProjects;
+    const db = getDatabase();
+    const projectsdb = ref(db, "gestaoempresa/projetos");
+    onValue(projectsdb, async (snapshot) => {
+        if (snapshot.val() === null) {
+            allProjects = [];
+        } else {
+            allProjects = snapshot.val();
+        };
+        const project = req.body;
+        project.business = req.user._id;
+        allProjects.push(project)
+        set(ref(db, "gestaoempresa/projetos"), allProjects);
+
+        return res.redirect("/dashboard/projetos?message=registered");
+    })
 });
 
 module.exports = router;
