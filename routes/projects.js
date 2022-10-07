20fe39f5e3891c1ea21ab4cb9941e6159b5066b5
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-07 15:05:36 -0500

    [ADDED] Dinamic renders

--- routes/projects.js
+++ routes/projects.js
@@ -5,24 +5,27 @@ const express = require("express"),
 const { authenticationMiddleware, authenticationMiddlewareTrueFalse } = require("../auth/functions/middlewares")
 
 router.get("/", (req, res, next) => {
-    if (!authenticationMiddlewareTrueFalse(req, res, next)) return res.redirect("/");
-    const db = getDatabase();
-    const projectsdb = ref(db, "gestaoempresa/projetos");
-    onValue(projectsdb, async (snapshot) => {
-        let projects;
-        if(snapshot.val() === null || snapshot.val() === undefined) {
-            projects = [];
-        } else {
-            projects = snapshot.val().filter(item => item.business === req.user._id);
-        }
-        const data = {
-            user: req.user,
-            projects,
-        };
-        res.render("pages/projects", data);
-    }, {
-        onlyOnce: true
-      });
+    if (authenticationMiddlewareTrueFalse(req, res, next)) {
+        const db = getDatabase();
+        const projectsdb = ref(db, "gestaoempresa/projetos");
+        onValue(projectsdb, async (snapshot) => {
+            let projects;
+            if(snapshot.val() === null || snapshot.val() === undefined) {
+                projects = [];
+            } else {
+                projects = snapshot.val().filter(item => item.business === req.user._id);
+            }
+            const data = {
+                user: req.user,
+                projects,
+            };
+            res.render("pages/projects", data);
+        }, {
+            onlyOnce: true
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 router.get("/adicionar", (req, res, next) => {
@@ -31,7 +34,6 @@ router.get("/adicionar", (req, res, next) => {
         user: req.user,
     };
     res.render("pages/projects/new", data);
-
 });
 
 router.post("/adicionar", (req, res, next) => {
@@ -45,6 +47,7 @@ router.post("/adicionar", (req, res, next) => {
         } else {
             allProjects = snapshot.val();
         };
+        console.log(req.body)
         const project = req.body;
         allProjects.push(project)
         set(ref(db, "gestaoempresa/projetos"), allProjects);
@@ -52,7 +55,7 @@ router.post("/adicionar", (req, res, next) => {
         return res.redirect("/dashboard/projetos?message=registered");
     }, {
         onlyOnce: true
-      })
+      });
 });
 
 module.exports = router;
