7916c915b2bf265fe803395a060c8f7e0c1c302d
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-18 10:43:59 -0500

    [ADDED] dynamic render complaint

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -63,10 +63,21 @@ router.get("/localizar/equipe", (req, res, next) => {
 
 router.get("/reclamacoes", (req, res, next) => {
     if (!authenticationMiddlewareTrueFalse(req, res, next)) return res.redirect("/");
-    const data = {
-        user: req.user,
-    };
-    res.render("pages/customers/complaint", data);
+    const db = getDatabase();
+    const projectsdb = ref(db, "gestaoempresa");
+    onValue(projectsdb, async (snapshot) => {
+        let complaint;
+        if(snapshot.val().complaint === null || snapshot.val().complaint === undefined) {
+            complaint = [];
+        } else {
+            complaint = snapshot.val().complaint.filter(item => item.ids.businessId === req.user._id);
+        }
+        const data = {
+            user: req.user,
+            complaint,
+        };
+        res.render("pages/customers/complaint", data);
+    })
 });
 
 
