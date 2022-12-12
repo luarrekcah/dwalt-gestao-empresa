02952ab03f5c8e7aaaa6d8d9c9d933c69000c8d3
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-12 18:49:30 -0500

    [added] logs

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const indexRouter = require("./routes/index"),
 accountRouter = require("./routes/account"),
-dashboardRouter = require("./routes/dashboard")
+logsRouter = require("./routes/logs"),
+dashboardRouter = require("./routes/dashboard"),
 projectsRouter = require("./routes/projects"),
 staffsRouter = require("./routes/staffs"),
 customersRouter = require("./routes/customers");
@@ -8,13 +9,18 @@ customersRouter = require("./routes/customers");
 const api = require("./routes/api");
 
 module.exports = (app) => {
-    //api
-    app.use("/api/v1", api);
+    //routes
     app.use("/", indexRouter);
     app.use("/conta", accountRouter);
+    app.use("/logs", logsRouter);
     app.use("/dashboard", dashboardRouter);
     app.use("/dashboard/projetos", projectsRouter);
     app.use("/dashboard/gerenciar/equipe", staffsRouter);
     app.use("/dashboard/gerenciar/clientes", customersRouter);
+    
+    //api
+    app.use("/api/v1", api);
+
+    //not found
     //app.use("*", notFoundRouter);
 };
