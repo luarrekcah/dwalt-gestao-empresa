a9e3f80ce8add2b52e52b5cdc2a4a0f6b460847b
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-15 16:09:06 -0500

    [ADDED] Login limiter

--- auth/local.js
+++ auth/local.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs"),
   localStrategy = require("passport-local").Strategy;
 const { getDatabase, ref, onValue } = require("@firebase/database");
-const { createLogs } = require("../database/users");
+const { createLogs, getItems } = require("../database/users");
+
+const moment = require("../services/moment");
 
 module.exports = async (passport) => {
   const db = getDatabase();
@@ -11,9 +13,9 @@ module.exports = async (passport) => {
 
     let users = [];
     snapshot.forEach(childSnapshot => {
-        let key = childSnapshot.key,
-            data = childSnapshot.val();
-        users.push({ key, data })
+      let key = childSnapshot.key,
+        data = childSnapshot.val();
+      users.push({ key, data })
     });
 
     console.log(`[LOG] Usuários atualizados em tempo real: ${users.length}`);
@@ -43,7 +45,7 @@ module.exports = async (passport) => {
     passport.use(
       new localStrategy(
         { usernameField: "email", passwordField: "password" },
-        (email, password, done) => {
+        async (email, password, done) => {
           if (users === null) return done(null, false);
           try {
             const user = findUser(email);
@@ -51,7 +53,27 @@ module.exports = async (passport) => {
             const isValid = bcrypt.compareSync(password, user.data.info.password);
             if (!isValid) return done(null, false);
             createLogs(user.key, "Login realizado");
-            return done(null, user);
+            const loginLimit = await getItems({ path: `gestaoempresa/business/${user.key}/config/login` });
+
+            if (loginLimit.hourSpecified[0] !== '') {
+              const nowHours = moment().format('LT').split(":")[0];
+              const nowMins = moment().format('LT').split(":")[1];
+              if ((Number(nowHours) >= Number(loginLimit.hourSpecified[0].split(":")[0])
+                && Number(nowHours) <= Number(loginLimit.hourSpecified[1].split(":")[0]))
+                && (Number(nowMins) >= Number(loginLimit.hourSpecified[0].split(":")[1])
+                  && Number(nowMins) <= Number(loginLimit.hourSpecified[1].split(":")[1]))
+              ) {
+                console.log("LOGIN EM HORARIO PERMITIDO")
+                return done(null, user);
+              } else {
+                console.log("LOGIN FORA DE HORARIO PERMITIDO")
+                return done(null, false);
+              }
+            } else {
+              console.log("HORARIO DE LOGIN NAO DEFINIDO")
+              return done(null, user);
+            }
+
           } catch (err) {
             console.log(err);
             return done(err, false);
