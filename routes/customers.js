05d65f3c6cc2caaabb517a5fb2beb398c48dff3f
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-20 13:51:53 -0500

    [added] edit customer data

--- routes/customers.js
+++ routes/customers.js
@@ -66,4 +66,26 @@ router.get("/visualizar/:id", async (req, res, next) => {
     res.render("pages/customers/see", data);
 });
 
+
+router.get("/editar/:id", async (req, res, next) => {
+    if (!authenticationMiddlewareTrueFalse(req, res, next)) return res.redirect("/");
+    const user = await getUser({ userId: req.user.key })
+    const customer = await getItems({ path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}` });
+    let message = null
+
+    const data = {
+        user,
+        customer,
+        message,
+    };
+    res.render("pages/customers/edit", data);
+});
+
+router.post("/editar/:id", async (req, res, next) => {
+    if (!authenticationMiddlewareTrueFalse(req, res, next)) return res.redirect("/");
+    updateItem({ path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}`, params: req.body });
+    createLogs(req.user.key, "Cliente atualizado.");
+    return res.redirect("/dashboard/clientes/visualizar/" + req.params.id + "?message=editado");
+})
+
 module.exports = router;
