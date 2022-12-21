420d1269ae74512e9b762149d488b0686aef5af6
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-21 14:33:16 -0500

    [fixed] error on update profile without update photo

--- routes/account.js
+++ routes/account.js
@@ -23,26 +23,44 @@ router.post("/", (req, res, next) => {
         const storage = getStorage();
         const { logoSrc, ownerName, mainLocation, phone, about, tokenGrowatt } = req.body;
         console.log(req.body);
-        const storageRef = ref(storage, `gestaoempresa/business/${req.user.key}/info/profile/logo.png`);
-        uploadString(storageRef, logoSrc, 'data_url').then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((downloadURL) => {
-                updateItem({
-                    path: `gestaoempresa/business/${req.user.key}/info/profile`, params: {
-                        logo: downloadURL,
-                        ownerName,
-                        mainLocation,
-                        phone,
-                        about,
-                    }
-                });
-                updateItem({
-                    path: `gestaoempresa/business/${req.user.key}/info`, params: {
-                        tokenGrowatt
-                    }
-                });
-                return res.redirect("/conta");
+        if (logoSrc.includes('http')) {
+            updateItem({
+                path: `gestaoempresa/business/${req.user.key}/info/profile`, params: {
+                    logo: logoSrc,
+                    ownerName,
+                    mainLocation,
+                    phone,
+                    about,
+                }
             });
-        });
+            updateItem({
+                path: `gestaoempresa/business/${req.user.key}/info`, params: {
+                    tokenGrowatt
+                }
+            });
+            return res.redirect("/conta");
+        } else {
+            const storageRef = ref(storage, `gestaoempresa/business/${req.user.key}/info/profile/logo.png`);
+            uploadString(storageRef, logoSrc, 'data_url').then((snapshot) => {
+                getDownloadURL(snapshot.ref).then((downloadURL) => {
+                    updateItem({
+                        path: `gestaoempresa/business/${req.user.key}/info/profile`, params: {
+                            logo: downloadURL,
+                            ownerName,
+                            mainLocation,
+                            phone,
+                            about,
+                        }
+                    });
+                    updateItem({
+                        path: `gestaoempresa/business/${req.user.key}/info`, params: {
+                            tokenGrowatt
+                        }
+                    });
+                    return res.redirect("/conta");
+                });
+            });
+        }
     } else {
         return res.redirect("/");
     }
