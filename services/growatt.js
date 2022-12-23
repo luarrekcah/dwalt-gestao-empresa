a1483019dafac540e95f7a4e0c2e67ca0d34ba4e
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-23 18:30:38 -0500

    [added] update historic consume in services/growatt

--- services/growatt.js
+++ services/growatt.js
@@ -24,6 +24,42 @@ const getData = async (dataB) => {
             });
             console.log("Token atualizado para " + dataB.data.info.documents.nome_fantasia);
         });
+
+        const growatt = await getItems({path: `gestaoempresa/business/${dataB.key}/growatt`});
+
+            projects.forEach(p => {
+                if(p.data.username_growatt !== '' && p.data.username_growatt !== undefined) {
+                    const username = p.data.username_growatt;
+                    const plant = growatt.plantList.data.data.plants.find(plant => plant.name === username);
+                    const data = new Date();
+    
+                    const now = moment(new Date());
+                    const date = moment(p.data.month_power.data.lastUpdate);
+                    const duration = moment.duration(now.diff(date));
+                    
+                    if (duration.asHours() <= 3.0) {
+                        axios.get("https://test.growatt.com/v1/plant/energy",
+                        {
+                            headers: { token: dataB.data.info.tokenGrowatt },
+                            params: {
+                                plant_id: plant.plant_id,
+                                start_date: plant.create_date,
+                                end_date: `${data.getFullYear()}-${data.getMonth() + 1}-${data.getDate()}`, 
+                                time_unit: 'month',
+                            }
+                        })
+                        .then(response => {
+                            const data = response.data;
+                            data.lastUpdate = getDate();
+                            updateItem({
+                                path: `gestaoempresa/business/${dataB.key}/projects/${p.key}/month_power`, params: { data }
+                            });
+                        });
+                    } else {
+                        return;
+                    } 
+                }
+            });
 }
 
 setInterval(async () => {
