8f84ce9ca269dfb3f5eb86c519aca2aada41286b
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-23 09:11:18 -0500

    checkpoint

--- utils/index.js
+++ utils/index.js
@@ -12,25 +12,33 @@ asaasAPI.config(params);
 module.exports = {
   subscriptionChecker: async (req) => {
     if (req.user === undefined) return { code: false, redirect: "/" };
+
     const user = await getUser({ userId: req.user.key });
-    if (user.data.asaasID === "" || user.data.asaasID === undefined)
+
+    if (user.data.asaasID === "" || user.data.asaasID === undefined) {
       return { code: false, redirect: "/pagamento" };
+    }
+
     if (
       user.data.subscriptionID === "" ||
       user.data.subscriptionID === undefined
-    )
+    ) {
       return { code: false, redirect: "/pagamento/assinatura" };
+    }
+
     const response = await asaasAPI.subscriptions.get(user.data.subscriptionID);
 
     const okStatuses = ["ACTIVE", "CONFIRMED", "RECEIVED", "RECEIVED_IN_CASH"];
 
-    if (response.data.deleted || !okStatuses.includes(response.data.status))
+    if (response.data.deleted || !okStatuses.includes(response.data.status)) {
       return { code: false, redirect: "/?message=subscription_error" };
-    else if (
+    } else if (
       !response.data.deleted &&
       okStatuses.includes(response.data.status)
-    )
-      return { code: true };
-    else return { code: false, redirect: "/?message=subscription_error" };
+    ) {
+      return { code: true, redirect: "/dashboard" };
+    } else {
+      return { code: false, redirect: "/?message=subscription_error" };
+    }
   },
 };
