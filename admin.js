5158b1168f79f4e5e274a5899d283876428e032d
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-03-11 10:24:26 -0500

    updated credentials admin login and removed unsed content

--- admin.js
+++ admin.js
@@ -1,7 +1,12 @@
 const admin = require('firebase-admin');
 try {
     admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
+        credential: admin.credential.cert({
+            projectId: process.env.project_id,
+            privateKeyId: process.env.private_key_id,
+            privateKey: process.env.private_key.replace(/\\n/g, '\n'),
+            clientEmail: process.env.client_email,
+        }),
         databaseURL: process.env.databaseURL
     });
     console.log('[LOG] Conectado ao ADMIN')
