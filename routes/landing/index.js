cbc17b50ef15ca36d7cfb1e8c59d6211a250ebdc
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-05-24 17:41:51 -0500

    [added] policy and terms

--- routes/landing/index.js
+++ routes/landing/index.js
@@ -6,4 +6,13 @@ router.get("/", async (req, res, next) => {
     res.render("pages/landingpage/index");
 });
 
+router.get("/politicas", async (req, res, next) => {
+    res.render("pages/landingpage/policy");
+});
+
+
+router.get("/termos", async (req, res, next) => {
+    res.render("pages/landingpage/terms");
+});
+
 module.exports = router;
