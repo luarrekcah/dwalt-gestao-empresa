07872066c0320d5cf5b6dde29b70fb8360a6f3c0
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-10-25 18:54:54 -0500

    a

--- routes/api/controllers/inverters.controller.js
+++ routes/api/controllers/inverters.controller.js
@@ -3,7 +3,7 @@ const { getAllItems, getItems } = require("../../../database/users");
 async function getGrowattData(req, res, next) {
   let { businessKey } = req.params;
 
-  const growattData = await getAllItems({
+  const growattData = await getItems({
     path: `gestaoempresa/business/${businessKey}/growatt`,
   });
 
