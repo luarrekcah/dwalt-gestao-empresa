4f9fff8a3cd1d12dc8da54e45a649cd92ada9368
luarrekcah <raulrodriguesdemoraes@gmail.com>
2024-09-21 19:38:35 -0500

    sincronização de proejtos

--- routes/kanban.js
+++ routes/kanban.js
@@ -13,45 +13,50 @@ router.get("/projects", async (req, res, next) => {
   })
   let kanbanData = await getItems({
     path: `gestaoempresa/business/${req.user.key}/kanban/projects`,
-  })
-
-  console.log(kanbanData[0]);
-
+  });
+  
   const projects = await getAllItems({
     path: `gestaoempresa/business/${req.user.key}/projects`,
-  })
-
-
+  });
+  
+  // Verificar se o kanbanData está vazio ou nulo
   if (!kanbanData || kanbanData.length === 0) {
-    const cards = await projects.map(pj => {
-      if (pj.data.status === "finalizado") return;
-      return {
-        id: pj.key,
-        content: pj.data.apelidoProjeto,
-        notes: "" // pj.data.customer.name
-      }
-    });
-
     kanbanData = [{
       id: 1,
       title: "Pendentes",
-      cards,
-    }]
-
-    updateItem({
-      path: `gestaoempresa/business/${req.user.key}/kanban`,
-      params: {
-        projects: kanbanData
-      }
-    })
+      cards: [],
+    }];
   }
-
-  /**
-   * [
-    { id: 101, content: "Tarefa 1", notes: "Anotações para tarefa 1" },
-    { id: 102, content: "Tarefa 2", notes: "Anotações para tarefa 2" }
-  ]
-   */
+  
+  // Mapeia os IDs dos projetos no Kanban
+  const kanbanProjectIds = kanbanData[0].cards.map(card => card.id);
+  
+  // Verifica se há projetos que não estão no Kanban e os adiciona
+  projects.forEach(pj => {
+    if (pj.data.status !== "finalizado" && !kanbanProjectIds.includes(pj.key)) {
+      // Se o projeto não está finalizado e não está no Kanban, adiciona ao Kanban
+      kanbanData[0].cards.push({
+        id: pj.key,
+        content: pj.data.apelidoProjeto,
+        notes: "", // pode ser pj.data.customer.name ou outra informação
+      });
+    }
+  });
+  
+  // Filtrar os cartões no Kanban e remover aqueles que foram finalizados
+  kanbanData[0].cards = kanbanData[0].cards.filter(card => {
+    const project = projects.find(pj => pj.key === card.id);
+    return project && project.data.status !== "finalizado";
+  });
+  
+  // Atualizar o Kanban com os dados filtrados e atualizados
+  await updateItem({
+    path: `gestaoempresa/business/${req.user.key}/kanban`,
+    params: {
+      projects: kanbanData,
+    },
+  });
+  
 
 
   const data = {
