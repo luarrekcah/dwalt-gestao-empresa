f6cd2d693ef1f1e56513334a628944b5e66b0cdb
luarrekcah <raulrodriguesdemoraes@gmail.com>
2024-11-30 10:30:02 -0500

    Adicionar registro de documentos no registro do cliente

--- routes/customers.js
+++ routes/customers.js
@@ -18,6 +18,7 @@ const {
   uploadString,
   getDownloadURL,
   getStorage,
+  deleteObject,
 } = require("@firebase/storage");
 
 router.get("/", async (req, res, next) => {
@@ -37,9 +38,9 @@ router.get("/", async (req, res, next) => {
           title: "Cliente Registrado!",
           description:
             "Deseja criar um projeto para o cliente?",
-            buttons: [
-              {text: "Adicionar Projeto", link: "/dashboard/projetos/adicionar"}
-            ]
+          buttons: [
+            { text: "Adicionar Projeto", link: "/dashboard/projetos/adicionar" }
+          ]
         };
         break;
     }
@@ -84,21 +85,65 @@ router.get("/adicionar", async (req, res, next) => {
 });
 
 router.post("/adicionar", async (req, res, next) => {
-  const customer = req.body;
-  customer.createdAt = getDate();
-  console.log(customer);
-  const key = await createItem({
-    path: `gestaoempresa/business/${customer.business}/customers`,
-    params: customer,
-  });
+  try {
 
-  console.log(key);
-  
-  createLogs(
-    req.user.key,
-    `Cliente ${customer.nomeComp || customer.nomeFantasia} registrado.`
-  );
-  return res.redirect("/dashboard/clientes?message=registrado");
+    const storage = getStorage();
+    const customer = req.body;
+    customer.createdAt = getDate();
+
+    let documents = [];
+    if (customer.base64Documents) {
+      documents = JSON.parse(customer.base64Documents);
+      delete customer.base64Documents;
+    }
+
+    const key = await createItem({
+      path: `gestaoempresa/business/${customer.business}/customers`,
+      params: customer,
+    });
+
+    for (const doc of documents) {
+      try {
+        const match = doc.match(/^data:(.*?);base64,/);
+
+        if (!match || match.length < 2) {
+          throw new Error("Formato de base64 inválido");
+        }
+
+        const mimeType = match[1];
+        const extension = mimeType.split("/")[1];
+
+        const path = `gestaoempresa/business/${req.user.key}/customers/${key}/docs/${new Date().getTime()}.${extension}`;
+        const storageRef = ref(storage, path);
+
+        const base64Data = doc.replace(/^data:.*;base64,/, "");
+
+        await uploadString(storageRef, base64Data, "base64").then((snapshot) => {
+          getDownloadURL(snapshot.ref).then((downloadURL) => {
+            createItem({
+              path: `gestaoempresa/business/${req.user.key}/customers/${key}/docs`,
+              params: {
+                path,
+                url: downloadURL,
+              },
+            });
+          });
+        });
+      } catch (error) {
+        console.log("Erro ao processar documento:", error);
+      }
+    }
+
+    createLogs(
+      req.user.key,
+      `Cliente ${customer.nomeComp || customer.nomeFantasia} registrado.`
+    );
+
+    return res.redirect("/dashboard/clientes?message=registrado");
+  } catch (error) {
+    console.log(error);
+    return res.redirect("/dashboard/clientes?message=error");
+  }
 });
 
 router.get("/visualizar/:id", async (req, res, next) => {
@@ -106,22 +151,22 @@ router.get("/visualizar/:id", async (req, res, next) => {
   const notifications = await getAllItems({
     path: `gestaoempresa/business/${req.user.key}/notifications`,
   });
-  const photos =
+  const docs =
     (await getAllItems({
-      path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/photos`,
+      path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/docs`,
     })) || [];
   const customer = await getItems({
     path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}`,
   });
   let message = null;
 
-  console.log(photos);
+  console.log(docs);
 
   const data = {
     user,
     customer,
     message,
-    photos,
+    photos: docs,
     notifications,
   };
   res.render("pages/customers/see", data);
@@ -130,24 +175,32 @@ router.get("/visualizar/:id", async (req, res, next) => {
 router.post("/visualizar/:id", async (req, res, next) => {
   const storage = getStorage();
   const data = req.body;
+
   switch (data.type) {
     case "ADD_PHOTO":
       const receivedData = JSON.parse(data.documentBase64);
+
       for (const image of receivedData) {
-        const path = `gestaoempresa/business/${req.user.key}/customers/${
-          req.params.id
-        }/photos/${new Date().getTime()}.jpg`;
-        const storageRef = ref(storage, path);
         try {
-          uploadString(storageRef, image, "data_url").then((snapshot) => {
-            console.log(snapshot);
+          // Extraindo a extensão do arquivo do base64
+          const match = image.match(/^data:(.*?);base64,/);
+          if (!match || match.length < 2) {
+            throw new Error("Formato de base64 inválido");
+          }
+          const mimeType = match[1];
+          const extension = mimeType.split("/")[1]; // Ex.: image/png -> png
+
+          // Definindo o caminho do arquivo com a extensão correta
+          const path = `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/docs/${new Date().getTime()}.${extension}`;
+          const storageRef = ref(storage, path);
+
+          // Removendo o prefixo base64 antes de enviar para o Firebase
+          const base64Data = image.replace(/^data:.*;base64,/, "");
+
+          await uploadString(storageRef, base64Data, "base64").then((snapshot) => {
             getDownloadURL(snapshot.ref).then((downloadURL) => {
-              console.log(downloadURL);
-              console.log(
-                `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/photos`
-              );
               createItem({
-                path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/photos`,
+                path: `gestaoempresa/business/${req.user.key}/customers/${req.params.id}/docs`,
                 params: {
                   path,
                   url: downloadURL,
@@ -156,9 +209,11 @@ router.post("/visualizar/:id", async (req, res, next) => {
             });
           });
         } catch (error) {
-          console.log(error);
+          console.log("Erro ao processar imagem:", error);
         }
       }
+
+      // Criando log de ação
       createLogs(req.user.key, "Imagens adicionadas para um cliente.");
       break;
   }
@@ -222,6 +277,41 @@ router.get("/checkExist/:cpf_cnpj", async (req, res, next) => {
       error: false,
       message: "user not found",
     });
+});
+
+router.delete("/deletedoc", async (req, res, next) => {
+  try {
+    const storage = getStorage();
+    const filePath = req.query.path;
+
+
+    if (!filePath) {
+      return res.status(400).send({ message: "Caminho do arquivo não fornecido." });
+    }
+
+    const fileRef = ref(storage, filePath);
+
+    await deleteObject(fileRef);
+
+    const pathSegments = filePath.split("/"); // Ex.: gestaoempresa/business/{user}/customers/{id}/docs/{file}
+    const customerId = pathSegments[4]; 
+    const docPath = `gestaoempresa/business/${pathSegments[2]}/customers/${customerId}/docs`;
+
+    const alldocs = await getAllItems({path: docPath});
+
+    const docToDel = alldocs.find(dc => dc.data.path === filePath);
+
+    await deleteItem({
+      path: `${docPath}/${docToDel.key}`,
+    });
+
+    createLogs(req.user.key, `Documento removido para o cliente ${customerId}.`);
+
+    return res.status(200).send({ message: "Documento removido com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao deletar documento:", error);
+    return res.status(500).send({ message: "Erro ao deletar documento." });
+  }
 });
 
 module.exports = router;
